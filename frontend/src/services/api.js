8c28a6911cbe537e81be7a59014d824786905443
nikhitamendu <nikhitamendu3612@gmail.com>
2026-03-05 18:58:42 +0530

    api added in axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
   //This file creates a centralized Axios instance that automatically attaches authentication tokens and connects React to backend APIs.
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL:"https://job-portal-backend-jtjz.onrender.com",
+  baseURL:"https://job-portal-backend-jtjz.onrender.com/api",
   withCredentials: true, // for refresh token cookie
 });
 
