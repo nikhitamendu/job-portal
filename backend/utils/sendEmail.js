eee7266faa01c817b21f5ffc1d3b95cac98d98b2
nikhitamendu <nikhitamendu3612@gmail.com>
2026-03-05 21:40:17 +0530

    fix nodemailer gmail service

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -25,16 +25,15 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, html) => {
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    // host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true,
+    service:"gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      rejectUnauthorized: false
     }
+    
   });
 
   await transporter.sendMail({
