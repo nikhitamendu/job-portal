ed22d72aac1b6d38eda2d57a7fc7747d7b63b6ca
nikhitamendu <nikhitamendu3612@gmail.com>
2026-03-06 07:38:28 +0530

    use env api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,42 @@
+// import axios from "axios";
+//   //This file creates a centralized Axios instance that automatically attaches authentication tokens and connects React to backend APIs.
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true, // for refresh token cookie
+// });
+
+// let accessToken = null;
+// // We DO NOT store access token in localStorage. we keep it in memory
+// // ✅ THIS IS CRITICAL
+// export const setAccessToken = (token) => {
+//   accessToken = token;
+// };
+
+// // Attach token to every request
+// api.interceptors.request.use(
+//   (config) => {
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+// //this runs before every req //React request → interceptor → attach JWT → backend
+// export default api;
 import axios from "axios";
-  //This file creates a centralized Axios instance that automatically attaches authentication tokens and connects React to backend APIs.
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // for refresh token cookie
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 let accessToken = null;
-// We DO NOT store access token in localStorage. we keep it in memory
-// ✅ THIS IS CRITICAL
+
 export const setAccessToken = (token) => {
   accessToken = token;
 };
 
-// Attach token to every request
 api.interceptors.request.use(
   (config) => {
     if (accessToken) {
@@ -22,5 +46,5 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
-//this runs before every req //React request → interceptor → attach JWT → backend
+
 export default api;
