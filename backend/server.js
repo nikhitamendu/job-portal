6af24cde52714ce62b6bdbd4441f0eb18c709ec8
nikhitamendu <nikhitamendu3612@gmail.com>
2026-03-06 11:12:40 +0530

    email options added

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,15 @@ const mongoose = require("mongoose"); //odm to interact with database using sche
 const cors = require("cors");//connect frontend //allows react different port to access baqckend
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-
 const app = express();  //this line creates backend server object //everu request from frontend first enters app
 app.set("trust proxy", 1); //for deployment for cookies
 const userRoutes = require("./routes/userRoutes");  //require means import the file use means register the routes in express
 const { GridFSBucket } = require("mongodb");
-
-
-/* =======================
-   GLOBAL MIDDLEWARE
-======================= */
 app.use(express.json());  //converts frontend json req in js object
-//supoose frontend sends email:nikjbnjmk backend recieves req.body.email
-
-// app.use(
-//   cors({
-//    origin: ["http://localhost:5173","https://job-portal-frontend-latest.onrender.com"],
-//     credentials: true,
-  // })   //react localhost:5173backend:local host 5000
-  //since frontend and backend run on different ports,browser blocks request due to some origing policy.so i enabled cors to all react to communicare with bACJEND
-// );
 const allowedOrigins = [
   "http://localhost:5173",
   // "https://job-portal-frontend-latest.onrender.com"
 ];
-
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -40,29 +24,16 @@ app.use(
     credentials: true
   })
 );
-app.use(cookieParser());  //used for authentication systems
-
-/* =======================
-   ROUTES
-======================= */    //attach all routes inside userRoutes to thr 
+app.use(cookieParser());  
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/protected", require("./routes/protectedRoutes"));
-
 app.use("/api/users", userRoutes);
 app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/applications", require("./routes/applicationRoutes"));
-
-
-
-/* =======================
-   SERVER START
-======================= */
 async function startServer() {
   try {
-    await mongoose.connect(process.env.MONGO_URI)   //we used async await because to connect with database it may take some time and we want to start the server only after the database is connected successfully.
-    
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB connected");
-
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
@@ -75,7 +46,6 @@ mongoose.connection.once("open", () => {
   const bucket = new GridFSBucket(mongoose.connection.db, {
     bucketName: "uploads"
   });
-
-  app.locals.bucket = bucket;   //stores files (resume )  using gridfs mongodb document =16mb,resume pfd are bigger so we store them as chunks
+  app.locals.bucket = bucket;
 });
 startServer();
